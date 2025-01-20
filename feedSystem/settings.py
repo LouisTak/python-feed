049db0052b00c5ac0ce28e0c5e183f6752f9b360
Louis Chan <louiscct922483@gmail.com>
2025-01-20 12:42:20 +0800

    feat: add swagger

--- feedSystem/settings.py
+++ feedSystem/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'rest_framework',
-	'api'
+	'api',
+	'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SWAGGER_SETTINGS = {
+	"JSON_EDITOR": True,
+}
