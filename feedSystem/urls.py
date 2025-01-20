d77f33e5d2151e5eeb6709dcd71f389dde600170
Louis Chan <louiscct922483@gmail.com>
2025-01-20 13:59:18 +0800

    feat: file structure changed

--- feedSystem/urls.py
+++ feedSystem/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -23,21 +24,15 @@ schema_view = get_schema_view(
     openapi.Info(
         title="Feed System API",
         default_version='v1',
-        description="API for the Feed System",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="louiscct922483@gmail.com"),
-        license=openapi.License(name="MIT License"),
+        description="API documentation for Feed System",
     ),
     public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('api/', include('api.urls')),
-]
-
-urlpatterns += [
-    path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+	path('api/feeds/', include('api.feed.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
