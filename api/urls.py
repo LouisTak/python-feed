7818249f392f3811e806bddefa72ca44813a1e07
Louis Chan <louiscct922483@gmail.com>
2025-01-20 12:42:05 +0800

    feat: use genericView instead of annotation in feed api

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import create_feed, get_feeds, feed_detail
+from .views import FeedsView
 
 urlpatterns = [
-	path('feeds/', get_feeds, name='get_feeds'),
-	path('feeds/create/', create_feed, name='create_feed'),
-	path('feeds/<int:pk>', feed_detail, name='feed_detail')
+	path('feeds/', FeedsView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,39 +1,34 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status	
 from .models import Feed
 from .serializer import FeedSerializer
+from rest_framework.generics import GenericAPIView
 
-@api_view(['GET'])
-def get_feeds(request):
-	feeds = Feed.objects.all()
-	serializer = FeedSerializer(feeds, many=True)
-	return Response(serializer.data)
+class FeedsView(GenericAPIView):
+	queryset = Feed.objects.all()
+	serializer_class = FeedSerializer
 
-@api_view(['GET','PUT', 'DELETE'])
-def feed_detail(request, pk):
-	try:
-		feed = Feed.objects.get(pk=pk)
-	except Feed.DoesNotExist:
-		return Response(status=status.HTTP_404_NOT_FOUND)
-
-	if request.method == 'GET':
-		serializer = FeedSerializer(feed)
+	def get(self, request, *args, **kwargs):
+		feeds = self.get_queryset()
+		serializer = self.get_serializer(feeds, many=True)
 		return Response(serializer.data)
-	elif request.method == 'PUT':
-		serializer = FeedSerializer(feed, data=request.data, partial=True)
+
+	def post(self, request, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	def put(self, request, *args, **kwargs):
+		feed = self.get_object()
+		serializer = self.get_serializer(feed, data=request.data, partial=True)
 		if serializer.is_valid():
 			serializer.save()
 			return Response(serializer.data)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-	elif request.method == 'DELETE':
+
+	def delete(self, request, *args, **kwargs):
+		feed = self.get_object()
 		feed.delete()
 		return Response(status=status.HTTP_204_NO_CONTENT)
-
-@api_view(['POST'])
-def create_feed(request):
-	serializer = FeedSerializer(data=request.data)
-	if serializer.is_valid():
-		serializer.save()
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
-	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
